4cd57bf3052f52c8b79c9f5324f18a830b7dc3a6
Chris Tilden <chris.tilden@gmail.com>
2018-04-12 01:17:51 -0700

    adds initial parser and unittest

--- java/src/main/java/com/christilden/lox/Lox.java
+++ java/src/main/java/com/christilden/lox/Lox.java
@@ -43,15 +43,25 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        // for now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        // stops if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
     private static void report(int line, String where, String message) {
---
+++ java/src/test/java/com/christilden/lox/ParserTest.java
@@ -0,0 +1,42 @@
+package com.christilden.lox;
+
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for AstPrinter.
+ */
+public class ParserTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ParserTest( String testName ) {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite( ParserTest.class );
+    }
+
+    /**
+     * Tests the parser.
+     */
+    public void testParser() {
+        int line = 1;
+        List<Token> tokens = Arrays.asList(new Token(TokenType.NUMBER, null, 123, line), new Token(TokenType.MINUS, "-", null, line), new Token(TokenType.NUMBER, null, 45.67, line), new Token(TokenType.EOF, "", null, line));
+
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        assertEquals(new AstPrinter().print(expression), "(- 123 45.67)");
+    }
+}
