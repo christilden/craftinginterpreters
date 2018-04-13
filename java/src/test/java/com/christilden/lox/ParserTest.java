c3bbd860bda96d291ba0ee0ed2e8e9309a2b54f2
Chris Tilden <chris.tilden@gmail.com>
2018-04-12 20:06:53 -0700

    enhancing test suite

--- java/src/test/java/com/christilden/lox/ParserTest.java
+++ java/src/test/java/com/christilden/lox/ParserTest.java
@@ -30,13 +30,25 @@ public class ParserTest extends TestCase {
     /**
      * Tests the parser.
      */
-    public void testParser() {
+    public void testParserSimple() {
         int line = 1;
-        List<Token> tokens = Arrays.asList(new Token(TokenType.NUMBER, null, 123, line), new Token(TokenType.MINUS, "-", null, line), new Token(TokenType.NUMBER, null, 45.67, line), new Token(TokenType.EOF, "", null, line));
+        List<Token> tokens = Arrays.asList(new Token(TokenType.NUMBER, null, 123.0, line), new Token(TokenType.MINUS, "-", null, line), new Token(TokenType.NUMBER, null, 45.67, line), new Token(TokenType.EOF, "", null, line));
 
         Parser parser = new Parser(tokens);
         Expr expression = parser.parse();
 
-        assertEquals(new AstPrinter().print(expression), "(- 123 45.67)");
+        assertEquals(new AstPrinter().print(expression), "(- 123.0 45.67)");
+    }
+
+    /**
+     * Tests the scanner and parser.
+     */
+    public void testScannerAndParserSimple() {
+        Scanner scanner = new Scanner("1 + 2 * 3");
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        assertEquals(new AstPrinter().print(expression), "(+ 1.0 (* 2.0 3.0))");
     }
 }
