5521925cc730794a29c0077acfdf402a47a20a50
Chris Tilden <chris.tilden@gmail.com>
2018-04-13 23:43:37 -0700

    initial function call work

--- java/src/main/java/com/christilden/lox/Interpreter.java
+++ java/src/main/java/com/christilden/lox/Interpreter.java
@@ -1,10 +1,25 @@
 package com.christilden.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
 
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+        });
+    }
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -196,6 +211,27 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         // unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected" + function.arity()
+                    + " arguments but got " + arguments.size() + ".");
+        }
+        return function.call(this, arguments);
     }
 
     private boolean isEqual(Object a, Object b) {
