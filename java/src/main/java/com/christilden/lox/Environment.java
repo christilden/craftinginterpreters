4169f3a14b84eac671b1f858d06f083a9cdf3545
Chris Tilden <chris.tilden@gmail.com>
2018-04-13 21:39:31 -0700

    adds code blocks
    code blocks are just a list of statements and a new environment scope,
    as well as a reference to the old scope

--- java/src/main/java/com/christilden/lox/Environment.java
+++ java/src/main/java/com/christilden/lox/Environment.java
@@ -4,7 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -15,12 +24,19 @@ class Environment {
             return values.get(name.lexeme);
         }
 
+        if (enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
