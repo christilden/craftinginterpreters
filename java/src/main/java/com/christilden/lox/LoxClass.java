d9e66478a984bc6507b7ee9478b89a567ff15463
Chris Tilden <chris.tilden@gmail.com>
2018-04-16 23:45:59 -0700

    adds class declarations

---
+++ java/src/main/java/com/christilden/lox/LoxClass.java
@@ -0,0 +1,17 @@
+package com.christilden.lox;
+
+import java.util.List;
+import java.util.Map;
+
+class LoxClass {
+    final String name;
+
+    LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
