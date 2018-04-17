bbb634dfd9e559d517b71b8db2874e38c115dd17
Chris Tilden <chris.tilden@gmail.com>
2018-04-16 23:56:23 -0700

    adds object instances and ability to get properties

--- java/src/main/java/com/christilden/lox/Interpreter.java
+++ java/src/main/java/com/christilden/lox/Interpreter.java
@@ -279,6 +279,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return function.call(this, arguments);
     }
 
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, "Only instances have properties.");
+    }
+
     private boolean isEqual(Object a, Object b) {
         if (a == null && b == null) return true;
         if (a == null) return false;
