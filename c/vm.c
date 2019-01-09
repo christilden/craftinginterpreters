8d8ba781beacb698dff6403220dd3bd46070d7cc
Chris Tilden <chris.tilden@gmail.com>
2019-01-08 21:49:03 -0800

    creates hash table implementation, adds hash funciton for strings, implements string interning to "deduplicate" strings

--- c/vm.c
+++ c/vm.c
@@ -137,9 +137,11 @@ static InterpretResult run() {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
