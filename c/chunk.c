f60ee0369400d23a86fbd0cf9e8e5b95cb1047e5
Chris Tilden <chris.tilden@gmail.com>
2019-12-04 19:44:27 -0800

    resolves some potential bugs from garbage collection.
    Temporarily adding/leaving values on the stack in order to ensure
    they are not swept away by the garbage collector

--- c/chunk.c
+++ c/chunk.c
@@ -35,6 +35,8 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 }
 
 int addConstant(Chunk* chunk, Value value) {
+    push(value);
     writeValueArray(&chunk->constants, value);
+    pop();
     return chunk->constants.count - 1;
 }
